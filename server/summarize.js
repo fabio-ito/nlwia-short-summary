72402d98c1067d5949173d897dd6cbd0b1ae68f5
fabio-ito <129807420+fabio-ito@users.noreply.github.com>
2023-09-16 22:23:49 -0300

    Add files via upload

---
+++ server/summarize.js
@@ -0,0 +1,25 @@
+import { pipeline } from "@xenova/transformers"
+import { summaryExample } from "./utils/summary.js"
+
+export async function summarize(text) {
+  //return summaryExample
+
+  try {
+    console.log("Resumindo...")
+
+    const generator = await pipeline(
+      "summarization",
+      "Xenova/distilbart-cnn-12-6"
+    )
+
+    const output = await generator(text)
+
+    console.log("Resumo concluído!")
+
+    return output[0].summary_text
+  } catch (error) {
+    console.log("Não foi possível resumir...", error)
+
+    throw new Error(error)
+  }
+}
